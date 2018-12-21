a09f1e009cde223bfeaa4f32221bfe509d1a2c45
Mike Morris <michaelmorrisg@gmail.com>
2018-12-20 19:22:18 -0700

    api fetches pokemon

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    model(){
+        $.get(`https://pokeapi.co/api/v2/pokemon/1/`).then(result => {
+            console.log(result)
+            return result
+        })
+    }
 });
