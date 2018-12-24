889e5b4996250710a7d985f5056e84c0b021b020
Mike Morris <michaelmorrisg@gmail.com>
2018-12-24 15:35:45 -0700

    button to toggle screen

--- app/routes/home.js
+++ app/routes/home.js
@@ -8,9 +8,13 @@ export default Route.extend({
         getPokemon(num){
             return $.get(`https://pokeapi.co/api/v2/pokemon/${num}/`).then(pokemon => {
             console.log(pokemon)
+            pokemon.id = pokemon
             this.controller.set('pokemon', pokemon)  
             return pokemon
             })
+        },
+        changeComponent(input){
+            this.controller.set('display', input)
         }
     }
 });
