b490bfa5114262025219c148c4ffb50993f8f4c9
Mike Morris <michaelmorrisg@gmail.com>
2018-12-24 15:41:24 -0700

    controller deletes type when new pokemon is fetched

--- app/routes/home.js
+++ app/routes/home.js
@@ -8,8 +8,9 @@ export default Route.extend({
         getPokemon(num){
             return $.get(`https://pokeapi.co/api/v2/pokemon/${num}/`).then(pokemon => {
             console.log(pokemon)
-            pokemon.id = pokemon
-            this.controller.set('pokemon', pokemon)  
+            pokemon.id = pokemon.types[0]
+            this.controller.set('pokemon', pokemon)
+            this.controller.set('display', null)  
             return pokemon
             })
         },
